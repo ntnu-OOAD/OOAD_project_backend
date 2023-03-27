76a8de3fb53046f341d1df552453677502a994a0
kabo chang <brian891125@gmail.com>
2023-03-27 17:30:49 +0800

    feat:create database from ERD

--- bookkeeping/api/admin.py
+++ bookkeeping/api/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import *
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Ledger)
+admin.site.register(LedgerAccess)
+admin.site.register(Record)
+admin.site.register(SharePay)
+admin.site.register(Receipt)
--- bookkeeping/api/models.py
+++ bookkeeping/api/models.py
@@ -2,11 +2,37 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.TextField(max_length=100)
-    email = models.TextField(max_length=200)
-    created = models.DateTimeField(auto_now_add=True)
-    class Meta:
-        db_table = "bookkeeping"
+    UserID = models.BigAutoField(primary_key=True)
+    Username = models.TextField(max_length=320 , default = "None")
+    Email = models.TextField(max_length=320 , default = "None")
+    CreateDate = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+class Ledger(models.Model):
+    LedgerID = models.BigAutoField(primary_key=True)
+    OwnerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    Type = models.TextField(max_length=320 , default = "None")
+    CreateDate = models.DateTimeField(auto_now_add=True)
+
+class LedgerAccess(models.Model):
+    LedgerID = models.ForeignKey(Ledger, on_delete=models.CASCADE)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE)
+    AccessLevel = models.TextField(max_length=320 , default = "None")
+
+class Record(models.Model):
+    RecordID = models.BigAutoField(primary_key=True)
+    LedgerID = models.ForeignKey(Ledger, on_delete=models.CASCADE)
+    ItemName = models.TextField(max_length=320 , default = "None")
+    ItemType = models.TextField(max_length=320 , default = "None")
+    Cost = models.DecimalField(max_digits=10, decimal_places=2)
+    Payby = models.TextField(max_length=320 , default = "None")
+
+class SharePay(models.Model):
+    RecordID = models.ForeignKey(Record, on_delete=models.CASCADE)
+    ShareUser = models.ForeignKey(User, on_delete=models.CASCADE)
+    ShouldPay = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Receipt(models.Model):
+    ReceiptID = models.BigAutoField(primary_key=True)
+    RecordID = models.ForeignKey(Record, on_delete=models.CASCADE)
+    BuyDate = models.DateTimeField(default = "None")
+    StatusCode = models.TextField(max_length=320 , default = "None")
