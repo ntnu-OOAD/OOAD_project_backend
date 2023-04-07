71a37f51ddc0dff85f133d65270ee00c3e995ddc
HsuanYu <93172849+hsuanyu414@users.noreply.github.com>
2023-04-07 23:39:54 +0800

    fix: createsuperuser function

--- bookkeeping/api/models.py
+++ bookkeeping/api/models.py
@@ -2,35 +2,36 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, nickname, password=None, **extra_fields):
+    def create_user(self, username, nickname, password):
         """
         Creates and saves a User with the given email and password.
         """
         if not username:
             raise ValueError('The username field must be set')
-        user = self.model(username=username, nickname=nickname, **extra_fields)
+        user = self.model(username=username, nickname=nickname)
         user.set_password(password)
         user.save(using=self._db)
         return user
-
-    def create_superuser(self, username, nickname, password=None, **extra_fields):
+    def create_superuser(self, username, nickname, password):
         """
         Creates and saves a superuser with the given email and password.
         """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, nickname, password, **extra_fields)
+        user = self.model(username=username, nickname=nickname)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=320, unique=True, blank=False)
     nickname = models.CharField(max_length=320, blank=False)
     create_date = models.DateTimeField(auto_now_add=True)
-    is_active = True
-    is_staff = None
-    groups = None
-    user_permissions = None
-    is_superuser  = None
-    last_login = None
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['nickname']
